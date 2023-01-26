bf144183f78fecaa7082d50b721b7d89141ea826
kameron <khr675@gmail.com>
2023-01-26 17:36:10 -0600

    jan. 26 at 5:36pm

---
+++ Assignments/A1/Duplicates.c
@@ -0,0 +1,35 @@
+//-----------------------------------------
+// NAME: Kameron Ronald
+// STUDENT NUMBER: 7880495
+// COURSE: COMP 2160, SECTION: A01
+// INSTRUCTOR: Saulo dos Santos
+// ASSIGNMENT: assignment 1, QUESTION: question 1
+// 
+// REMARKS: This program is a c implementation of Duplicates.java.
+//
+//-----------------------------------------
+#include <stdio.h>
+
+
+#define MAX_WORD_LENGTH 15
+
+//------------------------------------------------------
+// myRoutine
+//
+// PURPOSE: tell me what it does! 
+// INPUT PARAMETERS:
+// Describe those parameters which accept data values.
+// OUTPUT PARAMETERS:
+// Describe those parameters which return values.
+// Include the value returned by the routine if not void.
+// Some of these may be the same as the input
+// parameters.
+//------------------------------------------------------
+
+
+int main() {
+    
+
+    printf("end of process.");
+    return 0;
+}
---
+++ Assignments/A1/Solver.c
@@ -0,0 +1,17 @@
+//-----------------------------------------
+// NAME: Kameron Ronald
+// STUDENT NUMBER: 7880495
+// COURSE: COMP 2160, SECTION: A01
+// INSTRUCTOR: Saulo dos Santos
+// ASSIGNMENT: assignment 1, QUESTION: question 2
+// 
+// REMARKS: This program is a c implementation of Duplicates.java.
+//
+//-----------------------------------------
+#include <stdio.h>
+
+
+int main() {
+    printf("end of process.");
+    return 0;
+}
