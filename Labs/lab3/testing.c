3cfdd0baad83d9f250bd97cb03d847f3c27af501
kameron <khr675@gmail.com>
2023-02-28 21:02:38 -0600

    COMP 2160 (auto-commit at 21:02:38.18)

--- Labs/lab3/testing.c
+++ Labs/lab3/testing.c
@@ -57,11 +57,22 @@ void test_typical_cases()
 
     printf("Testing typical cases\n");
 
+    test_binary_search(0, 1, array1, 3);
     test_binary_search(1, 2, array1, 3);
+    test_binary_search(2, 3, array1, 3);
     test_binary_search(-1, 4, array1, 3);
+
     test_binary_search(3, 7, array2, 8);
+    test_binary_search(6, 26, array2, 8);
+    test_binary_search(-1, 24, array2, 8);
+
     test_binary_search(1, 0, array3, 3);
+
     test_binary_search(0, 1, array4, 1);
-    test_binary_search(2, -1, array5, 1);  // ?
+
+    test_binary_search(0, -3, array5, 4);
+    test_binary_search(2, -1, array5, 4);
+    test_binary_search(3, 0, array5, 4);
+    test_binary_search(-1, -4, array5, 4);
 
 }
