b6c16bd766f36a1a6029f23751f9305df9e221a5
kameron <khr675@gmail.com>
2023-02-28 20:30:31 -0600

    COMP 2160 (auto-commit at 20:30:31.28)

--- Labs/lab3/testing.c
+++ Labs/lab3/testing.c
@@ -3,10 +3,6 @@
 #include "binsearch.h"
 
 //-------------------------------------------------------------------------------------
-// CONSTANTS
-//-------------------------------------------------------------------------------------
-
-//-------------------------------------------------------------------------------------
 // VARIABLES
 //-------------------------------------------------------------------------------------
 static int tests_executed = 0;
@@ -37,25 +33,40 @@ int main()
 
 void test_binary_search(int expected, int key, int array[], int count)
 {
-  int result = binsearch(key, array, count);
-  
-  if (result == expected) {
-    printf("Passed: %d searching for '%d' in array length %d\n", result, key, count);
-  } else {
-    printf("FAILED: %d instead of %d searching for '%d' in array length %d\n", result, expected, key, count);
-    tests_failed++;
-  }
-  
-  tests_executed++;
+    int result = binsearch(key, array, count);
+
+    if (result == expected)
+    {
+        printf("Passed: %d searching for '%d' in array length %d\n", result, key, count);
+    }
+    
+    else
+    {
+        printf("FAILED: %d instead of %d searching for '%d' in array length %d\n", result, expected, key, count);
+        tests_failed++;
+    }
+
+    tests_executed++;
 }
 
 void test_typical_cases()
 {
-  int array1[] = { 1, 2, 3 };
-  
-  printf("Testing typical cases\n");
-  
-  test_binary_search(1, 2, array1, 3);
+    int array1[] = { 1, 2, 3 };
 
-  // test more stuff...
+    printf("Testing typical cases\n");
+
+    test_binary_search(1, 2, array1, 3);
+
+    // test more stuff...
+}
+
+void test_edge_cases()
+{
+    int array1[] = { 1, 2, 3 };
+
+    printf("Testing typical cases\n");
+
+    test_binary_search(1, 2, array1, 3);
+
+    // test more stuff...
 }
