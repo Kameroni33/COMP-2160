3045aa8740a4b6ff91baa67059d6058a9e00e896
kameron <khr675@gmail.com>
2023-02-28 21:16:02 -0600

    COMP 2160 (auto-commit at 21:16:02.27)

--- Labs/lab3/testing.c
+++ Labs/lab3/testing.c
@@ -54,25 +54,32 @@ void test_typical_cases()
     int array3[] = { 0, 0, 0 };
     int array4[] = { 1 };
     int array5[] = { -3, -2, -1, 0 };
+    
+    int array6[] = NULL;
+    int array7[] = { 3, 2, 1 };
+    int array8[] = { 2, 3, 1 };
 
     printf("Testing typical cases\n");
 
+    // valid tests
     test_binary_search(0, 1, array1, 3);
     test_binary_search(1, 2, array1, 3);
     test_binary_search(2, 3, array1, 3);
-    // test_binary_search(-1, 4, array1, 3);
-
     test_binary_search(3, 7, array2, 8);
     test_binary_search(6, 26, array2, 8);
-    // test_binary_search(-1, 24, array2, 8);
-
     test_binary_search(1, 0, array3, 3);
-
     test_binary_search(0, 1, array4, 1);
-
     test_binary_search(0, -3, array5, 4);
     test_binary_search(2, -1, array5, 4);
     test_binary_search(3, 0, array5, 4);
+
+    //invalid tests
+    // test_binary_search(-1, 4, array1, 3);
+    // test_binary_search(-1, 24, array2, 8);
+    // test_binary_search(-1, 1, array3, 3);
     // test_binary_search(-1, -4, array5, 4);
+    // test_binary_search(-1, 0, array6, 0);
+    // test_binary_search(-1, 1, array7, 3);
+    // test_binary_search(-1, 1, array8, 3);
 
 }
