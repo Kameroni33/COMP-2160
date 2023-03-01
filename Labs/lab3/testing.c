39edec8bc3e5003eb1bf1c49589abea6a50e49a3
kameron <khr675@gmail.com>
2023-02-28 20:58:17 -0600

    COMP 2160 (auto-commit at 20:58:17.27)

--- Labs/lab3/testing.c
+++ Labs/lab3/testing.c
@@ -52,17 +52,28 @@ void test_binary_search(int expected, int key, int array[], int count)
 void test_typical_cases()
 {
     int array1[] = { 1, 2, 3 };
+    int array2[] = { 1, 3, 4, 7, 9, 14, 26, 31 };
+    int array3[] = { 0, 0, 0 };
+    int array4[] = { 1 };
+    int array5[] = { -3, -2, -1, 0 };
 
     printf("Testing typical cases\n");
 
     test_binary_search(1, 2, array1, 3);
+    test_binary_search(-1, 4, array1, 3);
+    test_binary_search(3, 7, array2, 8);
+    test_binary_search(1, 0, array3, 3);
+    test_binary_search(0, 0, array4, 1);
+    test_binary_search(2, -1, array5, 1);
 
     // test more stuff...
 }
 
 void test_edge_cases()
 {
-    int array1[] = { 1, 2, 3 };
+    int array1[] = { 0 };
+    int array1[] = { -2, -1, 0 };
+    int array1[] = { 0, 0, 0 };
 
     printf("Testing edge cases\n");
 
