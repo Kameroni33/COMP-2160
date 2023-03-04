708d1c45d5962b5db8e9b0c4954cbc404622041b
kameron <khr675@gmail.com>
2023-03-04 11:30:32 -0600

    COMP 2160 (auto-commit at 11:30:32.37)

--- Assignments/A2/maze_start.c
+++ Assignments/A2/maze_start.c
@@ -91,7 +91,9 @@ void checkState();
 
 int main( int argc, char *argv[] )
 {
-    // because the 
+    // this statement is to suppress the compiler errors [-Werror,-Wunused-parameter]
+    int numArgs = argc;
+    char proName[] = argv[0];
 
     // printf("argc: %d\nargv: %s\n\n", argc, argv[0]);  // debug log
 
