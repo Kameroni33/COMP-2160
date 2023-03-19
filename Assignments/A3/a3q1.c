2f55c73f5a17ece2ebf6e694071722b8a535ed9d
kameron <khr675@gmail.com>
2023-03-19 15:36:52 -0500

    COMP 2160 (auto-commit at 15:36:51.43)

--- Assignments/A3/a3q1.c
+++ Assignments/A3/a3q1.c
@@ -2,11 +2,36 @@
 #include <assert.h>
 #include "orderedList.h"
 
+//=====================================================================================
+// Global Variables
+//=====================================================================================
+static int testsTotal;
+static int testsPassed;
+static int testsFailed;
 
+//=====================================================================================
+// Function Prototypes
+//=====================================================================================
+static void testOrderedList();
+
+//=====================================================================================
+// Function Definitions
+//=====================================================================================
 int main( int argc, char *argv[] )
 {
     printf("program: %s\nargc: %d\n\n", argv[0]+2, argc);
 
-    printf("\n\nEnd of Program.\n");
+    testOrderedList();
+
+    printf("testsTotal:  %d\n", testsTotal);
+    printf("testsPassed: %d\n", testsPassed);
+    printf("testsFailed: %d\n", testsFailed);
+
+    printf("\nEnd of Program.\n");
     return 0;
+}
+
+void testOrderedList()
+{
+    testsTotal++;
 }
