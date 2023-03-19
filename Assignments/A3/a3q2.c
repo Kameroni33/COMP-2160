65e1d562c39cddbf25648a9235ff686c2fc71e4a
kameron <khr675@gmail.com>
2023-03-19 15:14:24 -0500

    COMP 2160 (auto-commit at 15:14:24.41)

---
+++ Assignments/A3/a3q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+#include "orderedList.h"
+
+
+int main( int argc, char *argv[] )
+{
+    printf("program: %s\nargc:    %d\n\n", argv[0], argc);
+
+    printf("\n\nEnd of Program.\n");
+    return 0;
+}
