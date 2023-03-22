5f896ceee36c6ec71da699e7ae4f5a37c0256769
kameron <khr675@gmail.com>
2023-03-22 16:46:05 -0500

    COMP 2160 (auto-commit at 16:46:05.25)

--- Assignments/A3/orderedList.h
+++ Assignments/A3/orderedList.h
@@ -10,7 +10,6 @@ typedef struct LIST List;
 List *construct( );
 boolean insert( List *list, char *new_string );
 boolean find( List *list, char *target );
-int node_count( List *list );
 int size( List *list );
 void print( List *list );
 int traversals( );
