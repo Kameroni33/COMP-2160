48f507f70913e95747de221abebd29258035db26
kameron <khr675@gmail.com>
2023-03-23 14:03:36 -0500

    COMP 2160 (auto-commit at 14:03:36.25)

--- Assignments/A3/a3q2.c
+++ Assignments/A3/a3q2.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include <assert.h>
 #include "orderedList.h"
 
 #define MAX_WORD 100
 
+void removeChar(char *word, int index);
 
 int main( int argc, char *argv[] )
 {
@@ -15,27 +17,39 @@ int main( int argc, char *argv[] )
 
     construct(concordance);  // create concordance list
 
-    // read each line of the file
+    // read each 'word' of the file
     while ( (result = scanf("%s", word)) != EOF )
     {
+        // remove any whitespace or punctuation
+        for (int i = 0; i < strlen(word); i++)
+        {
+            // check if the current character is whitespace/punctuation
+            if (isspace(word[i]) || ispunct(word[i]))
+            {
+                removeChar(word, i);
+            }
+        }
+
+        // add the word to the concordance
         if ( !find(concordance, word) )
         {
             insert(concordance, word);
         }
     }
 
-    printf("Concordance:\n============================================================\n");
+    // print index, list, size, and traversals
     print(concordance);
-    printf("\n");
+    printf("Size: %d\n", size(concordance));
+    printf("Traversals: %d\n", traversals());
 
-    printf("Size:\n============================================================\n");
-    printf("%d\n\n", size(concordance));
-
-    printf("Traversals:\n============================================================\n");
-    printf("%d\n\n", traversals());
-
-    destroy(concordance);
+    destroy(concordance);  // clean up concordance list
 
     printf("\nEnd of Program.\n");
     return 0;
+}
+
+void removeChar(char *word, int index)
+{
+    // remove the char at 'index' by shifting the string in memory
+    memmove(&word[index], &word[index + 1], strlen(word) - index);
 }
