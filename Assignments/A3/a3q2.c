e131774ca7f0a6f5c94edbf95df2befffaa5a3cd
kameron <khr675@gmail.com>
2023-03-23 13:21:50 -0500

    COMP 2160 (auto-commit at 13:21:50.30)

--- Assignments/A3/a3q2.c
+++ Assignments/A3/a3q2.c
@@ -2,11 +2,43 @@
 #include <assert.h>
 #include "orderedList.h"
 
+#define MAX_WORD 20
 
+//=====================================================================================
+// Global Variables
+//=====================================================================================
+static int testsTotal;
+static int testsPassed;
+static int testsFailed;
+static int lastTraversals;
+
+//=====================================================================================
+// Function Prototypes
+//=====================================================================================
+void testOrderedList(char *words[], int numWords, int expectedInsertTraversals, int expectedFindTraversals);
+
+//=====================================================================================
+// Function Definitions
+//=====================================================================================
 int main( int argc, char *argv[] )
 {
     printf("program: %s\nargc: %d\n\n", argv[0]+2, argc);
 
-    printf("\n\nEnd of Program.\n");
+    List *concordance = construct();
+    char word[MAX_WORD];
+    int result;
+
+    construct(concordance);
+
+    while ( (result = scanf("%s", word)) != EOF )
+    {
+        printf("%s\n", word);
+    }
+
+    print(concordance);
+
+    destroy(concordance);
+
+    printf("\nEnd of Program.\n");
     return 0;
 }
