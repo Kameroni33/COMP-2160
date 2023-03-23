e240f71d9368b8626e83f5ab892861991e1961e0
kameron <khr675@gmail.com>
2023-03-23 15:23:24 -0500

    COMP 2160 (auto-commit at 15:23:24.19)

--- Assignments/A3/a3q1.c
+++ Assignments/A3/a3q1.c
@@ -11,12 +11,13 @@ static int testsTotal;
 static int testsPassed;
 static int testsFailed;
 static int lastTraversals;
+static int testsNum;
 
 //=====================================================================================
 // Function Prototypes
 //=====================================================================================
-int printTestHeader(char *words[], int numWords, int testNum);
-void testOrderedList(char *words[], int numWords, int expectedInsertTraversals, int expectedFindTraversals);
+void testOrderedList();
+void testNormalCase(char *words[], int numWords, int expectedInsertTraversals, int expectedFindTraversals);
 
 //=====================================================================================
 // Function Definitions
@@ -25,6 +26,14 @@ int main( int argc, char *argv[] )
 {
     printf("program: %s\nargc: %d\n\n", argv[0]+2, argc);
 
+    testOrderedList();
+
+    printf("\nEnd of Program.\n");
+    return 0;
+}
+
+void testOrderedList()
+{
     // Test Cases
     int testLen1 = 4;
     int testLen2 = 5;
@@ -33,125 +42,83 @@ int main( int argc, char *argv[] )
     char *testWords2[MAX_WORD] = {"apple", "calculus", "cow", "camp", "camel"};
     char *testWords3[MAX_WORD] = {"apple", "art", "avocado", "bee", "camp", "egg", "hello", "home", "puzzle", "zoo"};
 
-    testOrderedList(testWords1, testLen1, 3, 3);
-    testOrderedList(testWords2, testLen2, 4, 6);
-    testOrderedList(testWords3, testLen3, 13, 4);
+    printf("==============================\n");
+    ptintf("Testing Ordered List Library");
+    printf("==============================\n");
 
     printf("\n");
     printf("testsTotal:  %d\n", testsTotal);
     printf("testsPassed: %d\n", testsPassed);
     printf("testsFailed: %d\n", testsFailed);
-
-    printf("\nEnd of Program.\n");
-    return 0;
 }
 
 void testNormalCase(char *words[], int numWords, int expectedInsertTraversals, int expectedFindTraversals)
 {
-    boolean passed = true;
     int insertTraversals;
     int findTraversals;
+    int insertFails = 0;
+    int findFails = 0;
 
-    List *list = construct();
-
-}
-
-void testOrderedList(char *words[], int numWords, int expectedInsertTraversals, int expectedFindTraversals)
-{
-    int testLen1 = 4;
-    int testLen2 = 5;
-    int testLen3 = 10;
-    char *testWords1[MAX_WORD] = {"apple", "calculus", "cow", "camp"};
-    char *testWords2[MAX_WORD] = {"apple", "calculus", "cow", "camp", "camel"};
-    char *testWords3[MAX_WORD] = {"apple", "art", "avocado", "bee", "camp", "egg", "hello", "home", "puzzle", "zoo"};
-
-
-    boolean passed = true;
-    int insertTraversals;
-    int findTraversals;
-    int testNum = 0;
-
-    List *list = construct();
-
-    printf("==============================\n");
-    ptintf("Testing Ordered List Library");
-    printf("==============================\n");
-    
-    testNum = printTestHeader(testWords1, testLen1, )
-
-    for (int i = 0; i < numWords; i++)
-    {
-        boolean result = insert(list, words[i]);
-        if (!result)
-        {
-            printf("insert failed\n");
-            passed = false;
-        }
-        print(list);
-        printf("traversals: %d\n\n", traversals());
-    }
-
-    // check number of insert traversals
-    insertTraversals = traversals() - lastTraversals;
-    lastTraversals = traversals();
-    if (insertTraversals != expectedInsertTraversals)
-    {
-        passed = false;
-        printf("insert traversals is %d, expected %d\n", insertTraversals, expectedInsertTraversals);
-    }
-    
-
-    for (int i = 0; i < numWords; i++)
-    {
-        boolean result = find(list, words[i]);
-        if (!result)
-        {
-            printf("find failed\n");
-            passed = false;
-        }
-    }
-
-    // check number of find traversals
-    findTraversals = traversals() - lastTraversals;
-    lastTraversals = traversals();
-    if (findTraversals != expectedFindTraversals)
-    {
-        passed = false;
-        printf("find traversals is %d, expected %d\n", findTraversals, expectedFindTraversals);
-    }
-
-    if (size(list) != numWords)
-    {
-        passed = false;
-        printf("node count is %d, expected %d\n", size(list), numWords);
-    }
-
-    destroy(list);
-
-    // update counters
-    if (passed)
-    {
-        testsPassed++;
-    }
-    else
-    {
-        testsFailed++;
-    }
-
-    testsTotal++;
-    printf("\n");
-}
-
-
-int printTestHeader(char *words[], int numWords, int testNum)
-{
-    printf("Test %d: {", testNum);
+    // print test header
+    printf("Test %d: {", testsNum);
     for (int i = 0; i < numWords; i++)
     {
         printf("\"%s\"", words[i]);
         if (i < numWords - 1) printf(", ");
     }
     printf("}\n");
+    testsNum++;
 
-    return testNum + 1;
+    // create list
+    List *list = construct();
+
+    // insert words into list
+    for (int i = 0; i < numWords; i++)
+    {
+        boolean result = insert(list, words[i]);
+        if (!result)
+        {
+            insertFails++;
+        }
+    }
+    printf("%d insert failures\n", insertFails);
+
+    // check number of insert traversals
+    insertTraversals = traversals() - lastTraversals;
+    lastTraversals = traversals();
+    printf("%d insert traversals, expected %d\n", insertTraversals, expectedInsertTraversals);
+
+    // find words in list
+    for (int i = 0; i < numWords; i++)
+    {
+        boolean result = find(list, words[i]);
+        if (!result)
+        {
+            findFails++;
+        }
+    }
+    printf("%d find failures\n", findFails);
+
+    // check number of find traversals
+    findTraversals = traversals() - lastTraversals;
+    lastTraversals = traversals();
+    printf("%d find traversals, expected %d\n", findTraversals, expectedFindTraversals);
+
+    // destroy list
+    destroy(list);
+
+    // check if test passed
+    if (insertFails == 0 && findFails == 0 && insertTraversals == expectedInsertTraversals && findTraversals == expectedFindTraversals)
+    {
+        testsPassed++;
+        printf("Test Passed.\n");
+    }
+    else
+    {
+        testsFailed++;
+        printf("Test Failed.\n");
+    }
+    testsTotal++;
 }
+
+
