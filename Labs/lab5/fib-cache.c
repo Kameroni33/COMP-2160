78894977ded6379ce28b70d11dcdb0b835c87fd0
kameron <khr675@gmail.com>
2023-03-30 11:24:16 -0500

    COMP 2160 (auto-commit at 11:24:16.23)

--- Labs/lab5/fib-cache.c
+++ Labs/lab5/fib-cache.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CACHE_SIZE 42
+
 long fib(int n)
 {
-  long result;
+    long result;
   
-  if (n==0)
-    result = 0;
-  else if (n==1 || n==2)
-    result = 1;
-  else
-    result = fib(n-1) + fib(n-2);
+    if (n==0)
+        result = 0;
+    else if (n==1 || n==2)
+        result = 1;
+    else
+        // first 
+        result = fib(n-1) + fib(n-2);
   
-  return result;
+    return result;
 }
 
 int main( int argc, char *argv[] )
 {
-  // we really should check the input...
-  int fibNum = atoi(argv[1]);
-  
-  printf("The %d Fibonacci number is %ld\n", fibNum, fib(fibNum));
-  
-  return EXIT_SUCCESS;
+    // we really should check the input...
+    int fibNum = atoi(argv[1]);
+
+    // instantiate our 'cache' with all 0's
+    int cache[CACHE_SIZE] = {0};
+
+    for (int i = 0; i < CACHE_SIZE; i++)
+    {
+        printf("%d\n", cache[i]);
+    }
+    
+    printf("The %d Fibonacci number is %ld\n", fibNum, fib(fibNum));
+    
+    return EXIT_SUCCESS;
 }
