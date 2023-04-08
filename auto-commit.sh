b30ce311db120da884535a7cb26e44a1650ec041
kameron <khr675@gmail.com>
2023-04-08 01:04:20 -0500

    [auto-commit] COMP 2160

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
+git pull
 while true; do
     CHANGES=$(git status -s)
-
     if [[ -n "$CHANGES" ]]; then
         # Commit the changes
         git add -A
         git commit -m "[auto-commit] COMP 2160"
         git push
     fi
-
-  # Sleep for 5 seconds before checking again
-  sleep 5
+    sleep 5
 done
